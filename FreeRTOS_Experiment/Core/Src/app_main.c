665d864ae85bfbf23d8cb9dabc9de4e6df241f73
dcabrera1983-byte <dcabrera-byte@github>
2025-12-12 09:17:52 -0500

    Added portmacro.h to get portTICK_PERIOD_MS

--- FreeRTOS_Experiment/Core/Src/app_main.c
+++ FreeRTOS_Experiment/Core/Src/app_main.c
@@ -4,6 +4,7 @@
 
 #include "../../Middleware/Third-Party/FreeRTOS/Source/include/FreeRTOS.h"
 #include "../../Middleware/Third-Party/FreeRTOS/Source/include/task.h"
+#include "portmacro.h"
 #include "stm32f7xx_hal_gpio.h"
 
 void TaskLoop1(void *arguments);
@@ -12,7 +13,8 @@ void TaskLoop2(void *arguments);
 
 void APP_main_init(){
     
-    xTaskCreate(TaskLoop1, "Task 1", 128, NULL, 1, NULL);
+    xTaskCreate(TaskLoop1, "Task 1", 128, NULL, 5, NULL);
+    xTaskCreate(TaskLoop2, "Task 2", 128, NULL, 5, NULL);
 
     vTaskStartScheduler();
 }
@@ -22,6 +24,14 @@ void TaskLoop1(void *arguments){
 
     while(1){
         HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-        HAL_Delay(1000);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
+
+void TaskLoop2(void *arguments){
+
+    while(1){
+        HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+        vTaskDelay(500/portTICK_PERIOD_MS);
     }
 }
